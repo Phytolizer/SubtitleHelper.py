8dc449377e119cdbdf2ad698372e369e52ead8fb
Kyle Coffey <mariorules24@gmail.com>
2021-12-03 11:03:52 -0600

    extract main loop

--- main.py
+++ main.py
@@ -2,23 +2,27 @@ import speech_recognition as sr
 import pygame as pg
 
 
+def main_loop(screen, source):
+    while True:
+        # Go through pygame events
+        for event in pg.event.get():
+            # Check quit event
+            if event.type == pg.QUIT:
+                pg.quit()
+                exit()
+            # Check keydown event
+            elif event.type == pg.KEYDOWN:
+                pass
+
+        # Update display
+        screen.fill("black")
+        pg.display.update()
+
+
 if __name__ == "__main__":
     r = sr.Recognizer()
     pg.init()
     screen = pg.display.set_mode((500, 100))
 
     with sr.Microphone() as source:
-        while True:
-            # Go through pygame events
-            for event in pg.event.get():
-                # Check quit event
-                if event.type == pg.QUIT:
-                    pg.quit()
-                    exit()
-                # Check keydown event
-                elif event.type == pg.KEYDOWN:
-                    pass
-
-            # Update display
-            screen.fill("black")
-            pg.display.update()
+        main_loop(screen, source)
