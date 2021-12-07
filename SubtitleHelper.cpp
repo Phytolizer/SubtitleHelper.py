f63e481fbcf89d91ea4a23bc217888afe1833e0c
Kyle Coffey <mariorules24@gmail.com>
2021-12-07 00:02:12 -0600

    remove useless texture

--- SubtitleHelper.cpp
+++ SubtitleHelper.cpp
@@ -49,7 +49,6 @@ int main()
 	SDL_Window* window =
 	    SDL_CreateWindow("Subtitle Helper", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 640, 480);
 	bool run = true;
 	while (run)
 	{
@@ -69,11 +68,10 @@ int main()
 				break;
 			}
 		}
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderClear(renderer);
-		SDL_RenderCopy(renderer, texture, nullptr, nullptr);
 		SDL_RenderPresent(renderer);
 	}
-	SDL_DestroyTexture(texture);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	TTF_Quit();
