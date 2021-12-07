e4431251ca0d180e4c89d8b1e3496fb057d5815a
Kyle Coffey <mariorules24@gmail.com>
2021-12-07 00:00:32 -0600

    open audio device, fill a buffer for it

--- SubtitleHelper.cpp
+++ SubtitleHelper.cpp
@@ -1,8 +1,51 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <type_traits>
+#include <vector>
+
+class Audio
+{
+	std::vector<Uint8> m_buffer;
+
+  public:
+	void FillBuffer(const void* buffer, int length)
+	{
+		size_t oldSize = m_buffer.size();
+		m_buffer.resize(oldSize + length);
+		SDL_memcpy(m_buffer.data() + oldSize, buffer, length);
+	}
+};
+
+void AudioInputCallback(void* userData, Uint8* stream, int len)
+{
+	auto* audio = reinterpret_cast<Audio*>(userData);
+	audio->FillBuffer(stream, len);
+}
+
+static_assert(std::is_same_v<decltype(&AudioInputCallback), SDL_AudioCallback>,
+              "AudioInputCallback is not a SDL_AudioCallback");
 
 int main()
 {
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+	TTF_Init();
+	TTF_Font* font24 = TTF_OpenFont("FreeSans.ttf", 24);
+	if (font24 == nullptr)
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to open FreeSans.ttf: %s", TTF_GetError());
+		return 1;
+	}
+	Audio audio;
+	SDL_AudioSpec wantIn;
+	SDL_zero(wantIn);
+	wantIn.freq = 44100;
+	wantIn.format = AUDIO_F32;
+	wantIn.channels = 1;
+	wantIn.samples = 4096;
+	wantIn.callback = AudioInputCallback;
+	wantIn.userdata = &audio;
+	SDL_AudioDeviceID device = SDL_OpenAudioDevice(nullptr, 1, &wantIn, nullptr, SDL_AUDIO_ALLOW_ANY_CHANGE);
+	SDL_PauseAudioDevice(device, 0);
 	SDL_Window* window =
 	    SDL_CreateWindow("Subtitle Helper", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
@@ -22,6 +65,7 @@ int main()
 	SDL_DestroyTexture(texture);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
+	TTF_Quit();
 	SDL_Quit();
 	return 0;
 }
